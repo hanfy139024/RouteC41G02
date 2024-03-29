f34e0414da94a96044181b972acc385e78e56ba9
mahmoud dv asp <mhmode1235@gmail.com>
2024-03-29 02:32:20 +0200

    Add project files.

---
+++ RouteC41G02PL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RouteC41G02PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
