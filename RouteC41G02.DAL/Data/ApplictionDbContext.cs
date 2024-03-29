f3f97e2a0eb21f686fb4696db6ef18ddff6462d6
mahmoud dv asp <mhmode1235@gmail.com>
2024-03-29 04:01:01 +0200

    Part 03 3-Layers Arch. Pattern - Data Access Layer

---
+++ RouteC41G02.DAL/Data/ApplictionDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RouteC41G02.DAL.Data.Configuration;
+using RouteC41G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteC41G02.DAL.Data
+{
+    internal class ApplictionDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        
+            
+            =>optionsBuilder.UseSqlServer("Server=.;Database =MVCApplictionG02;Trusted_Connection=True;MultipleActiveResultsets=False");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfiguration());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+
+
+        public DbSet<Department>Departments { get; set; }
+
+
+
+
+    }
+
+}
---
+++ RouteC41G02.DAL/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteC41G02.DAL.Models
+{
+    internal class Department
+    {
+
+        public int Id { get; set; }
+        [Required]//(ErrorMessage ="name is error ya hamde")]
+        public string Name { get; set; }
+        [Required]
+        public string Code { get; set; }
+        public DateTime Dateofcreation { get; set; }
+
+         
+    }
+}
